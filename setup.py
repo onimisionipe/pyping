d91c1d62dd7cc4ca46789de9024f05a912fb0942
Socketubs <geoffrey@lehee.name>
2012-05-26 02:48:13 +0200

    Clean setup.py

--- setup.py
+++ setup.py
@@ -7,48 +7,33 @@ from setuptools import setup
 PACKAGE_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 def get_authors():
-    authors = []
-    try:
-        f = file(os.path.join(PACKAGE_ROOT, "AUTHORS"), "r")
-        for line in f:
-            if not line.strip().startswith("*"):
-                continue
-            if "--" in line:
-                line = line.split("--", 1)[0]
-            authors.append(line.strip(" *\r\n"))
-        f.close()
-        authors.sort()
-    except Exception, err:
-        authors = "[Error: %s]" % err
-    return authors
+	authors = []
+	try:
+		f = file(os.path.join(PACKAGE_ROOT, "AUTHORS"), "r")
+		for line in f:
+			if not line.strip().startswith("*"):
+				continue
+			if "--" in line:
+				line = line.split("--", 1)[0]
+			authors.append(line.strip(" *\r\n"))
+		f.close()
+		authors.sort()
+	except Exception, err:
+		authors = "[Error: %s]" % err
+	return authors
 
 with open('README.md') as file:
-    long_description = file.read()
+	long_description = file.read()
 
 setup(
-    name='pyping',
-    version='0.0.1',
-    description='A pure python ICMP ping implementation using raw sockets.',
-    long_description=long_description,
-    author=get_authors(),
-    maintainer="Socketubs",
-    maintainer_email="geoffrey@lehee.name",
-    url='https://github.com/socketubs/pyping/',
-    keywords="ping icmp network latency",
+	name='pyping',
+	version='0.0.1',
+	description='A pure python ICMP ping implementation using raw sockets.',
+#	long_description=long_description,
+	author=get_authors(),
+	author_email="geoffrey@lehee.name",
+	url='https://github.com/socketubs/pyping/',
+	keywords="ping icmp network latency",
 	packages = ['pyping'],
-    scripts=["scripts/pyping"],
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "Intended Audience :: Education",
-        "Intended Audience :: End Users/Desktop",
-        "License :: OSI Approved :: GNU General Public License (GPL)",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Topic :: Internet",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Topic :: System :: Networking :: Monitoring",
-    ],
-    test_suite="tests",
+	scripts=["scripts/pyping"]
 )
